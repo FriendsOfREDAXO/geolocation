bb2af518b958b69e77931d54b1e780be0ca3c680
christophboecker <christoph_boecker@t-online.de>
2022-01-09 13:39:20 +0100

    Zusätzliche Box-Methoden

--- lib/calc/box.php
+++ lib/calc/box.php
@@ -25,9 +25,13 @@ use \Location\Bounds;
 
 class InvalidBoxParameter extends InvalidParameter {}
 
-
 class Box
 {
+	const HOOK_NW = 1;
+	const HOOK_NE = 2;
+	const HOOK_SE = 3;
+	const HOOK_SW = 4;
+	const HOOK_CE = 5;
 
 	/**
      * @var \Location\Bounds
@@ -322,6 +326,7 @@ class Box
 	 *	 Liefert ein Array mit den Eck-Koordinaten der Box
 	 *		[ 0 => [0=>latNW,1=lngNW], 1=>[0=>latSE,lngSE] ]
 	 *
+	 *   @param null|int		Anzahl Nachkommastellen; default: alle
      *   @return array
      */
 	public function latLng( ?int $precision=null ): array
@@ -338,6 +343,7 @@ class Box
 	 *	 Liefert ein Array mit den Eck-Koordinaten der Box
 	 *		[ 0 => [0=>lngNW,1=latNW], 1=>[0=>lmgSE,latSE] ]
 	 *
+	 *   @param null|int		Anzahl Nachkommastellen; default: alle
      *   @return array
      */
 	public function lngLat( ?int $precision=null ): array
@@ -349,8 +355,31 @@ class Box
 	}
 
 	/**
+     *   Die Breite der Box als Abstand der Längengrade
+	 *
+	 *   @param null|int		Anzahl Nachkommastellen; default: alle
+     *   @return array
+     */
+	public function width( ?int $precision=null ): float
+	{
+		return abs( $this->east() - $this->west() );
+	}
+
+	/**
+     *   Die Höhe der Box als Abstand der Breitengrade
+	 *
+	 *   @param null|int		Anzahl Nachkommastellen; default: alle
+     *   @return array
+     */
+	public function height( ?int $precision=null ): float
+	{
+		return abs( $this->north() - $this->south() );
+	}
+
+	/**
 	 *   Abschnitt für einen geoJSON-Datensatz: als zwei Punkte.
 	 *
+	 *   @param null|int		Anzahl Nachkommastellen; default: alle
 	 *   @return array
 	 */
 	public function geoJSONMultipoint( ?int $precision=null ): array
@@ -364,6 +393,7 @@ class Box
 	/**
 	 *   Abschnitt für einen geoJSON-Datensatz: als geschlossenes Polygon.
 	 *
+	 *   @param null|int		Anzahl Nachkommastellen; default: alle
 	 *   @return array
 	 */
 	public function geoJSONPolygon( ?int $precision=null ): array
@@ -384,6 +414,7 @@ class Box
 
 	/**
      *   Außenradius, also Kreis um den Mittelpunkt mit den Radius "halbe Diagonale"
+	 *   Funktioniert auch bei nicht-quadratischen Boxen.
 	 *
      *   @return float
      */
@@ -393,13 +424,18 @@ class Box
 	}
 
 	/**
-     *   Innenradius, also Kreis um den Mittelpunkt mit den Radius "halbe Distanz Nord/Süd"
+     *   Innenradius, also Kreis um den Mittelpunkt mit den Radius "halbe Distanz Nord/Süd" oder
+	 *	 "halbe Distanz Ost/West". Es wird der kürzere Weg herangezogen, da es sonst bei nicht-
+	 *   quadratischen Boxen schwierig wird.
 	 *
      *   @return float
      */
 	public function innerRadius(): float
 	{
-		return $this->nw->distanceTo( $this->southWest() ) / 2;
+		$sw = $this->southWest();
+		$distanceSN = $this->nw->distanceTo( $sw );
+		$distanceWE = $this->se->distanceTo( $sw );
+		return min( $distanceSN,$distanceWE ) / 2;
 	}
 
 	/**
@@ -437,17 +473,15 @@ class Box
 			if( !($point instanceof Point) ) {
 				throw new InvalidBoxParameter( InvalidBoxParameter::BOXEXTEND );
 			}
-			if( $this->contains( $point ) ) {
-				continue;
+			if( !$this->contains( $point ) ) {
+				$south[] = $point->lat();
+				$north[] = $point->lat();
+				$west[] = $point->lng();
+				$east[] = $point->lng();
 			}
-			$south[] = $point->lat();
-			$north[] = $point->lat();
-			$west[] = $point->lng();
-			$east[] = $point->lng();
 		}
 
-		if( $south ) {
-
+		if( $south ) { // es reicht aus, nur ein Array zu prüfen, alle sind gleich groß
 			$south[] = $this->south();
 			$north[] = $this->north();
 			$west[] = $this->west();
@@ -462,6 +496,96 @@ class Box
 			$this->se = Point::byLatLng( [$south,$east] );
 			$this->bounds = new Bounds( $this->nw->coord(), $this->se->coord() );
 		}
+		return $this;
+	}
+
+	/**
+     *   Erweitert die Box so, dass der angegebene Punkt innerhalb der Box liegt
+	 *
+	 *	 Sofern der neue Punkt dazu führt, dass die Box 180° oder mehr breit werden würde,
+	 *	 bricht die Methode mit einer Exception ab.
+	 *   Im Erfolgsfall sind $this->nw, $this->se und $this->bounds anschließend neu gesetzt.
+	 *
+	 *   @param float			Umrechnungsfaktor für Höhe (und ggf. Länge)
+	 *   @param null|float		Umrechnungsfaktor für Breite (oder null für Übernahme Höhenfaktor)
+	 *   @param int				Referenzpunkt, default: Center
+     *   @return \Geolocation\Calc\Box
+     */
+	public function resizeBy ( float $factorLat, ?float $factorLng=null, int $reference=self::HOOK_CE ): self
+	{
+		if( null === $factorLng ) {
+			$factorLng = $factorLat;
+		}
+
+		// Faktoren müssen > 0 sein.
+		if( $factorLat <= 0 ) {
+			throw new InvalidBoxParameter( InvalidBoxParameter::BOXRESIZELAT, [$factorLat] );
+		}
+
+		if( $factorLng <= 0 ) {
+			throw new InvalidBoxParameter( InvalidBoxParameter::BOXRESIZELNG, [$factorLng] );
+		}
+
+		// Abkürzung: wenn Faktor 1 dann unverändert
+		if( abs( $factorLat - 1 ) < 0.0000001 && abs( $factorLng - 1 ) < 0.0000001) {
+			return $this;
+		}
+
+		$deltaLat = $this->height() * $factorLat;
+		$deltaLng = $this->width() * $factorLng;
+
+		switch ($reference ) {
+			case self::HOOK_NW:
+				$north = $this->north();
+				$west = $this->west();
+				$south = $north - $deltaLat;
+				$east = $west + $deltaLng;
+				break;
+			case self::HOOK_NE:
+				$north = $this->north();
+				$east = $this->east();
+				$south = $north - $deltaLat;
+				$west = $east - $deltaLng;
+				break;
+			case self::HOOK_SE:
+				$south = $this->south();
+				$east = $this->east();
+				$north = $south + $deltaLat;
+				$west = $east - $deltaLng;
+				break;
+			case self::HOOK_SW:
+				$south = $this->south();
+				$west = $this->west();
+				$north = $south + $deltaLat;
+				$east = $west + $deltaLng;
+				break;
+			case self::HOOK_CE:
+				$deltaLat = $deltaLat / 2;
+				$deltaLng = $deltaLng / 2;
+				$center = $this->center();
+				$south = $center->lat() - $deltaLat;
+				$north = $center->lat() + $deltaLat;
+				$west = $center->lng() - $deltaLng;
+				$east = $center->lng() + $deltaLng;
+				break;
+			default:
+				throw new InvalidBoxParameter( InvalidBoxParameter::BOXRESIZE, [$reference] );
+		}
+
+		// passen die neuen Grenzen?
+		// Lat nur normalisieren, Lng als Fehler melden
+		if( 180 <= ($east-$west) ) {
+			throw new InvalidBoxParameter( InvalidBoxParameter::BOX2WIDE, [$east,$west,$east-$west] );
+		}
+		$north = Math::normalizeLatitude( $north );
+		$south = Math::normalizeLatitude( $south );
+
+		// neue Koordinaten übernehmen
+
+		$this->nw = Point::byLatLng( [$north,$west] );
+		$this->se = Point::byLatLng( [$south,$east] );
+		$this->bounds = new Bounds( $this->nw->coord(), $this->se->coord() );
+
 		return $this;
 	}
 
