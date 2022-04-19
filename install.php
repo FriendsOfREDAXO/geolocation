c310d90f60808c1061cf74898a66db13c52c8453
christophboecker <christoph_boecker@t-online.de>
2022-04-19 18:42:36 +0200

    Installation: Konstanten werden nicht gesetzt (#45)
    Eigentlich werden einige Konstanten nur in die boot.php (`define(...)`) geschrieben. Aber ....  beim Kompilieren der Assest wird bereits darauf zurückgegriffen. Da zu dem Zeitpunkt bei einer Installation die Konstanten aus boot.php noch nicht aktiviert sind, gibt es eine Warning im Log und falsch generiertes JS. Ein ähnliches Problem ergibt sich bei Re-Installation. In dem Fall existieren zwar die Konstanten, ein neuer Wert us der Re-Installation wird aber nicht übernommen.
    
    Der PR löst beide Probleme, in dem die während der (Re-)Installation ein Array mit den Konstanten ausdrücklich an compileAssets übergeben wird. Dort werden die neuen Werte priorisiert ggü. evtl schon vorhandenen Konstanten herangezogen

--- install.php
+++ install.php
@@ -182,8 +182,10 @@ try {
     // Ausgewählte Vorgabewerte als "define(...)" in die boot.php schreiben
     $defines = PHP_EOL;
     $config['Geolocation\\LOAD'] = $config['scope']['load'];
+    $definedValues = [];
     foreach( $config as $k=>$v ) {
         if( 'Geolocation\\' !== substr($k,0,12) ) continue;
+        $definedValues[$k] = $v;
         if( is_string($v) ) {
             $v = "'$v'";
         } elseif( is_bool($v) ) {
@@ -199,7 +201,7 @@ try {
 
     // Die JS/CSS-Dateien neu kompilieren, um Instanz-eigene Erweiterungen und Parameter
     // aus data/addons/geolocation einzubinden
-    \Geolocation\config_form::compileAssets( __DIR__.'/' );
+    \Geolocation\config_form::compileAssets( __DIR__.'/',$definedValues );
     $msg[] = $this->i18n( 'install_assets_prepared' );
 
     // Den Ordner 'data/addons/geolocation/assets' falls vorhanden in den Ordner 'assets/addons/geolocation' kopieren
