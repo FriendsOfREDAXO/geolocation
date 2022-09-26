ffbb07e2b7afc4fa3587b1835824bad3a10affa9
christophboecker <christoph_boecker@t-online.de>
2022-09-26 21:02:47 +0200

    Rexstan-Überarbeitung, Code-Style

--- pages/index.php
+++ pages/index.php
@@ -1,56 +1,82 @@
 <?php
 /**
- * Allgemeine Add-Einstiegsseite (page-Rahmen)
- *
- *  @package geolocation
- *
- *  @var \rex_addon $this
+ * Allgemeine Addon-Einstiegsseite (page-Rahmen).
  */
 
-//nur hier im BE relevante Konfigurationen einlesen und daraus weitere Hilfswerte für Formulare setzen
-$config = array_merge(
-    \rex_file::getConfig( \rex_path::addonData(\Geolocation\ADDON,'config.yml'), [] ),
-    \rex_file::getConfig( \rex_path::addon(\Geolocation\ADDON,'/install/config.yml'), [] ),
-);
-define( 'Geolocation\ZOOM_MIN', $config['zoom_min'] );
-define( 'Geolocation\ZOOM_MAX', $config['zoom_max'] );
-define( 'Geolocation\PROXY_ONLY', !$config['scope']['mapset'] );
+namespace Geolocation;
 
-// Anzeige / Scope beschränkt auf Proxy/Cache?
-if( \Geolocation\PROXY_ONLY ){
-    $subpages = \rex_be_controller::getPageObject(\rex_be_controller::getCurrentPagePart(1))->getSubpages();
-    if( isset($subpages['mapset']) ){
-        $subpages['mapset']->setHidden(true);
+use rex_addon;
+use rex_api_function;
+use rex_be_controller;
+use rex_extension;
+use rex_file;
+use rex_path;
+use rex_url;
+use rex_view;
+
+use function assert;
+use function define;
+
+/**
+ *  @var rex_addon $this
+ */
+
+/**
+ * Die im Backend relevanten Konfigurationen (Instanz und allgemein) werden eingelesen und daraus
+ * weitere Hilfswerte für Formulare gesetzt.
+ */
+$config = array_merge(
+    rex_file::getConfig(rex_path::addonData(ADDON, 'config.yml'), []),
+    rex_file::getConfig(rex_path::addon(ADDON, '/install/config.yml'), []),
+);
+define('Geolocation\ZOOM_MIN', $config['zoom_min']);
+define('Geolocation\ZOOM_MAX', $config['zoom_max']);
+define('Geolocation\PROXY_ONLY', !$config['scope']['mapset']);
+
+/**
+ * für die nächsten Schritte vorab das Seitenobject bereitstellen.
+ */
+$thisPage = rex_be_controller::getCurrentPageObject();
+assert(null !== $thisPage); // STAN: sonst "Cannot call method getParent() on rex_be_page|null."
+$mainPage = $thisPage->getParent() ?? $thisPage;
+
+/**
+ * Bei Systemen, die auf "nur Proxy" konfiguriert sind, wird die Kartensatz-Seite ausgeblendet.
+ */
+if (PROXY_ONLY) {
+    $subPage = $mainPage->getSubpage('mapset');
+    if (null !== $subPage) {
+        $subPage->setHidden(true);
     }
 }
 
-// Button "Delete Cache" konfigurieren (Referenziere auf die aktuelle Seite statt Systemstandard)
-$page = \rex_be_controller::getPageObject('geolocation');
-if( $page ){
-    $page = $page->getSubpage('clear_cache');
-    if( $page ){
-        $href = \rex_url::backendController([
-            'page' => \rex_be_controller::getCurrentPageObject()->getFullKey(),
-            'rex-api-call' => 'geolocation_clearcache'
-        ], false);
-        $page->setHref( $href );
-    }
+/**
+ * Den Button für die Delete-Cache-Seite so konfigurieren, dass man anschließend wieder
+ * auf diese Seite zurückkommt.
+ */
+$page = $mainPage->getSubpage('clear_cache');
+if (null !== $page) {
+    $href = rex_url::backendController([
+        'page' => $thisPage->getFullKey(),
+        'rex-api-call' => 'geolocation_clearcache',
+    ], false);
+    $page->setHref($href);
 }
 
 // Title und Submenü erzeugen/ausgeben
-echo \rex_view::title( $this->i18n('geolocation_title') );
+echo rex_view::title($this->i18n('geolocation_title'));
 
 // ggf. vorhandene API Messages ausgeben
 echo rex_api_function::getMessage();
 
 // Liste 'rex_geolocation_mapset' um eine Action zum Löschen des Layer-Cache erweitern
-\rex_extension::register('YFORM_DATA_LIST_ACTION_BUTTONS','\Geolocation\mapset::YFORM_DATA_LIST_ACTION_BUTTONS');
+rex_extension::register('YFORM_DATA_LIST_ACTION_BUTTONS', '\Geolocation\mapset::YFORM_DATA_LIST_ACTION_BUTTONS');
 
 // Liste 'rex_geolocation_layer' um eine Action zum Löschen der Layer-Caches erweitern
-\rex_extension::register('YFORM_DATA_LIST_ACTION_BUTTONS','\Geolocation\layer::YFORM_DATA_LIST_ACTION_BUTTONS');
+rex_extension::register('YFORM_DATA_LIST_ACTION_BUTTONS', '\Geolocation\layer::YFORM_DATA_LIST_ACTION_BUTTONS');
 
 // Liste 'rex_geolocation_layer' mit geänderter Sortierung
-\rex_extension::register('YFORM_DATA_LIST_QUERY','\Geolocation\layer::YFORM_DATA_LIST_QUERY');
+rex_extension::register('YFORM_DATA_LIST_QUERY', '\Geolocation\layer::YFORM_DATA_LIST_QUERY');
 
 // Und nun die aktuelle Seite anzeigen
-\rex_be_controller::includeCurrentPageSubPath();
+rex_be_controller::includeCurrentPageSubPath();
