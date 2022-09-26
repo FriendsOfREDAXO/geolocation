d5052fc30fe1076493b3252ec835c3dfaadb5e91
christophboecker <christoph_boecker@t-online.de>
2022-09-26 23:40:16 +0200

    Rexstan-Überarbeitung, Code-Style (#56)

--- pages/config.php
+++ pages/config.php
@@ -1,16 +1,17 @@
 <?php
 /**
- * Basiskonfiguration
- *
- * @package geolocation
- *
- * @var \rex_addon $this
+ * Basiskonfiguration.
  */
 
-$form = \Geolocation\config_form::factory( \Geolocation\ADDON );
+namespace Geolocation;
 
-$fragment = new \rex_fragment();
+use rex_fragment;
+use rex_i18n;
+
+$form = config_form::factory(ADDON);
+
+$fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
-$fragment->setVar('title', \rex_i18n::msg('geolocation_config'), false);
+$fragment->setVar('title', rex_i18n::msg('geolocation_config'), false);
 $fragment->setVar('body', $form->get(), false);
 echo $fragment->parse('core/page/section.php');
