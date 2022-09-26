398122add54c792b612236702f5c479026b6cd67
christophboecker <christoph_boecker@t-online.de>
2022-09-26 23:40:39 +0200

    Rexstan-Überarbeitung, Code-Style (#58)

--- update.php
+++ update.php
@@ -1,17 +1,15 @@
 <?php
-
-namespace Geolocation;
-
 /**
  * Ab dieser Version ist der Cronjob-Type in rex_cronjob von `Geolocation\cronjob` in `Geolocation\Cronjob` geändert.
  * Datenbank anpassen, wenn die aktuelle Version kleiner ist diese.
  */
 
+namespace Geolocation;
+
 // TODO: Funktionstest
 
 use rex;
 use rex_addon;
-use rex_file;
 use rex_sql;
 use rex_version;
 
@@ -19,14 +17,14 @@ use rex_version;
  * @var rex_addon $this
  */
 
-$conf = rex_file::getConfig(__DIR__ . '/package.yml');
-$newVersion = $conf['version'] ?? '2.0.0';
-
-if (rex_version::compare($newVersion, $this->getVersion(), '>')) {
+if (rex_version::compare('2.0.0', $this->getVersion(), '>')) {
     $sql = rex_sql::factory();
     $sql->setTable(rex::getTable('cronjob'));
     $sql->setValue('type', 'Geolocation\\Cronjob');
     $sql->setWhere('BINARY type=?', ['Geolocation\\cronjob']);
-    $sql->addGlobalUpdateFields(rex::getUser()->getLogin());
+    $user = rex::getUser();
+    if (null !== $user) {
+        $sql->addGlobalUpdateFields($user->getLogin());
+    }
     $sql->update();
 }
