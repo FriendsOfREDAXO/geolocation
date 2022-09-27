797cda6435c567b53aa8a6ebbaf6ada98f122c8c
christophboecker <christoph_boecker@t-online.de>
2022-09-27 10:35:53 +0200

    Rexstan-Überarbeitung, Code-Style

--- fragments/geolocation_rex_map.php
+++ fragments/geolocation_rex_map.php
@@ -1,51 +1,67 @@
 <?php
 /**
- * Fragment zum Aufbau des rex-map-HTML
+ * Fragment zum Aufbau des rex-map-HTML.
  *
  * Es finden nur formale Prüfungen statt. Fehlende Werte werden nicht durch Defaults ersetzt.
  * Hier wird aus Eingangsdaten HTML gebaut, mehr nicht.
  *
  * <rex-map dataset="..." mapset="..." map="..." ... ></rex-map>
- *
- * @package geolocation
+ */
+
+namespace Geolocation;
+
+use rex_fragment;
+use rex_string;
+
+use function count;
+use function is_array;
+use function is_string;
+
+/**
+ * @var rex_fragment $this
  */
 
 $classes = [];
 $attributes = [];
 
 // dataset muss ein Array sein
-if( isset($this->dataset) && is_array($this->dataset) ) {
+if (isset($this->dataset) && is_array($this->dataset)) {
     $attributes['dataset'] = json_encode($this->dataset);
 }
 
 // mapset muss ein Array sein
-if( isset($this->mapset) && is_array($this->mapset) ) {
+if (isset($this->mapset) && is_array($this->mapset)) {
     $attributes['mapset'] = json_encode($this->mapset);
 }
 
 // map (Kartenoptionen) muss ein Array sein
-if( isset($this->map) && is_array($this->map) ) {
+if (isset($this->map) && is_array($this->map)) {
     $attributes['map'] = json_encode($this->map);
 }
 
 // Klassen hinzufügen (string oder array)
-if( isset($this->class) ) {
+if (isset($this->class)) {
     $class = $this->class;
-    if( is_string($class) && $class) $class = explode(' ',$class);
-    if( is_array($class) && $class ) $classes = array_merge($classes,$class);
+    if (is_string($class)) {
+        $class = explode(' ', $class);
+    }
+    if (is_array($class)) {
+        $classes = array_merge($classes, $class);
+    }
 }
-if( $classes ) {
+$classes = array_filter($classes, 'trim');
+if (0 < count($classes)) {
     $attributes['class'] = array_unique($classes);
 }
 
 // sonstige Attribute hinzufügen (class,dataset,mapset,map ignorieren)
 
-if( isset($this->attributes) && is_array($this->attributes) && count($this->attributes) ) {
+if (isset($this->attributes) && is_array($this->attributes) && 0 < count($this->attributes)) {
     $attributes = array_merge(
         $attributes,
-        array_diff_key($this->attributes,['mapset'=>null,'dataset'=>null,'map'=>null,'class'=>null])
+        array_diff_key($this->attributes, ['mapset' => null, 'dataset' => null, 'map' => null, 'class' => null])
     );
 }
 
 // HTML-Tag generieren
-echo '<rex-map',\rex_string::buildAttributes($attributes),'></rex-map>';
+echo '<rex-map',rex_string::buildAttributes($attributes),'></rex-map>';
