0e8f5d6c8b8dadc657ec0bfbf782c5e77e63d0a9
christophboecker <christoph_boecker@t-online.de>
2022-12-13 14:55:14 +0100

    update.php wiederhergestellt (#117)
    Versehentlich in #113 gelöscht statt nur den überflüssigen Code zu entfernen

---
+++ update.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Bei Update Weiterleitung auf install.php
+ */
+
+namespace FriendsOfRedaxo\Geolocation;
+
+use rex_addon;
+
+/**
+ * @var rex_Addon $this
+ */
+
+$this->includeFile('install.php');
