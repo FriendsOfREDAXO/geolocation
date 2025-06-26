428e2b9b35d8260b3ca5af3e759f0ecadd6cc5b3
christophboecker <christoph_boecker@t-online.de>
2025-06-26 10:08:40 +0200

    Revert d2a6730

--- pages/yform.php
+++ pages/yform.php
@@ -76,14 +76,7 @@ if ('' !== $wrapper_class) {
     echo '<div class="',$wrapper_class,'">';
 }
 
-/**
- * Weiche für YForm 5.0
- */
-if(is_file(rex_path::addon('yform', 'pages/manager.data_edit.php'))) {
-    include rex_path::addon('yform', 'pages/manager.data_edit.php');
-} else {
-    include rex_path::plugin('yform', 'manager', 'pages/data_edit.php');
-}
+include rex_path::plugin('yform', 'manager', 'pages/data_edit.php');
 
 if ('' !== $wrapper_class) {
     echo '</div>';
